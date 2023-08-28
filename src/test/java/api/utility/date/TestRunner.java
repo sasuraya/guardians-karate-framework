031e2af151293dda43fb1f88e11b2cdd298ff5c1
sasuraya <surayasadat@gmail.com>
2023-08-28 00:37:07 -0400

    adding the new changes

--- src/test/java/api/utility/date/TestRunner.java
+++ src/test/java/api/utility/date/TestRunner.java
@@ -4,12 +4,12 @@ import com.intuit.karate.junit5.Karate;
 
 public class TestRunner {
 	
-	// by adding this we are anotation karate
+	//By adding this we are annotation karate
 	@Karate.Test
 	public Karate runTest() {
 		//address for our feature files and tag
 		return Karate.run("classpath:features")
-				.tags("@Regrastion");
+				.tags("Smoke");
 				 
 		//return Karate.run("classpath:features").tags("@Regression,@Smoke");
 	}
