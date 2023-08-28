046df251a0ece9f6c5f170dcdac96d0c8623a583
sasuraya <surayasadat@gmail.com>
2023-08-28 13:29:13 -0400

    final update

--- src/test/java/api/utility/date/RegressionRunner.java
+++ src/test/java/api/utility/date/RegressionRunner.java
@@ -2,14 +2,14 @@ package api.utility.date;
 
 import com.intuit.karate.junit5.Karate;
 
-public class TestRunner {
+public class RegressionRunner {
 	
 	//By adding this we are annotation karate
 	@Karate.Test
 	public Karate runTest() {
 		//address for our feature files and tag
 		return Karate.run("classpath:features")
-				.tags("Smoke");
+				.tags("@Regression");
 				 
 		//return Karate.run("classpath:features").tags("@Regression,@Smoke");
 	}
